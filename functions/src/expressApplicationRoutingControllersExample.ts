823af931ce430ca7d4ecdd635a5e80047067afcc
0xlino <13621351+0xlino@users.noreply.github.com>
2024-02-11 23:45:29 +0000

    Add cron job functionality

--- functions/src/expressApplicationRoutingControllersExample.ts
+++ functions/src/expressApplicationRoutingControllersExample.ts
@@ -6,6 +6,16 @@ import { useExpressServer, useContainer } from 'routing-controllers';
 import { LoggingMiddleware } from './middleware/logging';
 import { CustomErrorHandler } from './customError';
 import { ThingController } from './thing/thing.controller';
+import { registerController as registerCronJobs, useContainer as cronUseContainer } from 'cron-decorators';
+import { ExampleCronJob } from './cron/CronJob';
+
+const _registerCronJobs = (appConfig: any): any => {
+    if (!appConfig.cronJobsEnabled) {
+      return false;
+    }
+
+    return registerCronJobs([ExampleCronJob]);
+}
 
 /**
  * Why this way and not nestjs, that becuase using routing-controllers is a lot more flexible 
@@ -23,6 +33,13 @@ expressApplication.get("/", (req: Request, res: Response) => {
 });
 
 useContainer(Container);
+cronUseContainer(Container);
+
+_registerCronJobs({
+    cronJobsEnabled: true
+});
+
+console.log('Cron Jobs Registered');
 
 useExpressServer(expressApplication,{
     controllers: [ThingController],
